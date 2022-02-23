c0620c3a34540a3ee7962ece9a2d828dee94095c
stonecoldfox <40811721+Kelsyn@users.noreply.github.com>
2022-02-23 04:39:18 +0000

    RPN truncates towards 0

--- index.js
+++ index.js
@@ -2,27 +2,27 @@ function evalRPN(tokens){
   var tempArray = [];
   for (var i = 0; i < tokens.length; i++){
     if(tokens[i] === "+"){
-    tempArray.push(tempArray.pop() + tempArray.pop());
-    continue;
+      tempArray.push(tempArray.pop() + tempArray.pop());
+      continue;
     } else if(tokens[i] === "-"){
-    tempArray.push(tempArray.pop() - tempArray.pop());
-    continue;
+      tempArray.push(tempArray.pop() - tempArray.pop());
+      continue;
     } else if (tokens[i] === "*"){
-    tempArray.push(tempArray.pop() * tempArray.pop());
-    continue;
+      tempArray.push(tempArray.pop() * tempArray.pop());
+      continue;
     } else if (tokens[i] === "/"){
-    var first = tempArray.pop();
-    var second = tempArray.pop();
-    var divided = second / first;
-    tempArray.push(Math.floor(divided));
-    continue;
+      var first = tempArray.pop();
+      var second = tempArray.pop();
+      var divided = second / first;
+      if(divided > 0){
+        tempArray.push(Math.floor(divided));
+      } else{
+        tempArray.push(Math.ceil(divided));
+      }
+      continue;
     } else {
     tempArray.push(Number(tokens[i]));
     }
   }
   return tempArray;
 }
-
-
-// var testArray = ["4","13","5","/","+"];
-// console.log(evalRPN(testArray));
